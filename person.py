7446fa59cd4fceeea40f283999cdece00971ecf2
pya-h <p4ya.py@gmail.com>
2024-10-18 18:41:13 +0000

    load data completed

--- person.py
+++ person.py
@@ -2,13 +2,25 @@
 
 class Person:
 
-    def __init__(self, id: int, firstname: str, lastname: str, email: str, location: str) -> None:
+    def __init__(self, id: int, firstname: str, lastname: str, email: str, city: str) -> None:
         self.id: int = id
         self.firstname: str = firstname
         self.lastname: str = lastname
-        self.location: str = location
+        self.city: str = city
         self.email: str = email
+        self.owner_id: int | None = None
 
     @property
     def fullname(self):
         return f'{self.firstname} {self.lastname}'
+    
+    def allocate(self, owner_id: int):
+        if self.owner_id is not None:
+            raise ValueError('Person\'s data already have an owner!')
+        self.owner_id = owner_id
+
+    def reallocate(self, owner_id: int):
+        self.owner_id = owner_id
+    
+    def release(self):
+        self.owner_id = None
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import socket
 from threading import Thread
 import json
 from typing import List, Dict
+import csv
 
 class ServerClient:
     def __init__(self, id: int, address: socket._RetAddress, socket: socket.socket) -> None:
@@ -33,9 +34,23 @@ class Server:
 
         while i < arg_count - 1:
             payload[args[i]] = args[i + 1]
-        return payload
+        return json.dumps(payload)
 
-    def __init__(self, port: int) -> None:
+    @staticmethod
+    def loadDataTable(filename: str) -> List[Dict[str, str|int|float]]:
+        with open(filename, mode='r') as file:
+            source = csv.reader(file)
+            header = next(source)
+
+            table = []
+            for row in source:
+                dict_row = {}
+                for index, column in enumerate(header):
+                    dict_row[column.lower()] = row[index]
+                table.append(dict_row)
+            return table
+
+    def __init__(self, port: int, source_filename:str) -> None:
         if port in Server.servers:
             raise Exception(f'There is another server running on {HOST}:{port}')
         self.host = HOST
@@ -44,6 +59,16 @@ class Server:
         self.clients: Dict[int, ServerClient] = {}
         self.running = False
         self.last_client_id = 0
+        self.source_filename = source_filename
+        self.data: List[Person] = []
+    
+    def load_data(self):
+        try:
+            data_table = Server.loadDataTable(self.source_filename)
+            self.data = list(map(lambda row: Person(int(row['id']), row['firstname'], row['lastname'], row['email'], row['city']), data_table))
+        except Exception as ex:
+            print('Server could not load data:', ex, '\n\nServer continues running, but has no data to distribute to clients')
+            self.data.clear();
 
     def listen(self):
         self.socket.bind((self.host, self.port))
@@ -62,8 +87,9 @@ class Server:
     def handle_client(self, client: ServerClient):
         print(f'{client.address} is trying to connect ...')
         self.clients[client.id] = client
-        client.socket.sendall(json.dumps({'type': ServerCommandType.CONNECT, 'id': client.id}))
+        client.socket.sendall(Server.createCommand(ServerCommandType.CONNECT, 'id', client.id))
 
+        self.try_to_allocate_data(client)
         while True:
             data = client.socket.recv(DATA_LENGTH)
 
@@ -79,3 +105,6 @@ class Server:
 
         client.socket.close()
         del self.clients[client.id]
+
+    def try_to_allocate_data(self, client: ServerClient):
+        pass
